0a0d87f978939199065894b4b5be2b86f3ff1d24
Manas Kshatriya <manaskshatriya01@gmail.com>
2023-07-26 01:37:57 +0530

    google Outh setup

--- app.js
+++ app.js
@@ -6,13 +6,65 @@ const connectDB = require("./db/connect");
 const userRouter = require("./routes/userRouter/user.router");
 const expenseRouter = require("./routes/expenseRouter/expense.router");
 
+const passport = require('passport');
+
+
 app.use(express.json());
 app.use("/api/", userRouter);
 app.use("/api/expense", expenseRouter);
 
+//auth
+const path = require('path');
+const session = require("express-session");
+require('./controllers/auth/auth');
+
+
+app.use(session({
+  secret: 'secret',
+  resave: false,
+  saveUninitialized: false,
+  cookie: {secure: false }
+}))
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+function isLoggedIn(req,res,next){
+  req.user ? next() : res.status(401).send('Unauthorized')
+}
+
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
+
+
+app.get('/auth/google',
+  passport.authenticate('google', { scope:
+      [ 'email', 'profile' ] }
+));
+
+app.get( '/auth/google/callback',
+    passport.authenticate( 'google', {
+        successRedirect: '/auth/google/success',
+        failureRedirect: '/auth/google/failure'
+}));
+
+app.get('/auth/google/success', (req, res) => {
+
+  res.json(req.user)
+}); 
+
+app.get('/auth/google/failure', isLoggedIn ,  (req, res) => {
+  res.send('failure')
+});
+
+app.use('/auth/logout', (req, res) => {
+  req.session.destroy();
+  console.log('logged out');
+  res.redirect('/');
+})
+
 
 const port = process.env.PORT || 5000;
 const start = async () => {
---
+++ controllers/auth/auth.js
@@ -0,0 +1,29 @@
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth2").Strategy;
+const User = require("../../models/User/User");
+require("dotenv").config();
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: "http://localhost:5000/auth/google/callback",
+      passReqToCallback: true,
+    },
+    function (request, accessToken, refreshToken, profile, done) {
+      // User.find({ googleId: profile.id ,  }, function (err, user) {
+      //   return done(err, user);
+      // });
+      done(null, profile);
+    }
+  )
+);
+
+passport.serializeUser(function (user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function (user, done) {
+  done(null, user);
+});
